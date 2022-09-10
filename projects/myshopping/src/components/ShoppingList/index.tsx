78e85f0e15a8758e1011c16dd6d603394c475e8e
renatosouzaBR <natoicm@gmail.com>
2022-09-10 09:06:31 -0300

    feat: add fetch products

--- projects/myshopping/src/components/ShoppingList/index.tsx
+++ projects/myshopping/src/components/ShoppingList/index.tsx
@@ -1,18 +1,40 @@
-import React, { useState } from 'react';
-import { FlatList } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { Alert, FlatList } from "react-native";
+import firestore from "@react-native-firebase/firestore";
 
-import { styles } from './styles';
-import { Product, ProductProps } from '../Product';
-
-import { shoppingListExample } from '../../utils/shopping.list.data';
+import { styles } from "./styles";
+import { Product, ProductProps } from "../Product";
 
 export function ShoppingList() {
-  const [products, setProducts] = useState<ProductProps[]>(shoppingListExample);
+  const [products, setProducts] = useState<ProductProps[]>([]);
+
+  async function fetchProducts() {
+    try {
+      const productsCollection = firestore().collection("products");
+      const subscribe = productsCollection.onSnapshot((querySnapshot) => {
+        const data = querySnapshot.docs.map(
+          (doc) =>
+            ({
+              id: doc.id,
+              ...doc.data(),
+            } as ProductProps)
+        );
+
+        setProducts(data);
+      });
+    } catch (error) {
+      Alert.alert("Ops", "Não foi possível carregar a lista de produtos");
+    }
+  }
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
   return (
     <FlatList
       data={products}
-      keyExtractor={item => item.id}
+      keyExtractor={(item) => item.id}
       renderItem={({ item }) => <Product data={item} />}
       showsVerticalScrollIndicator={false}
       style={styles.list}
