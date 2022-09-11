8a4cccc81abf88712e40d2acbe1838f6417a2bd9
renatosouzaBR <natoicm@gmail.com>
2022-09-10 22:42:56 -0300

    feat: add update and delete product function

--- projects/myshopping/src/components/Product/index.tsx
+++ projects/myshopping/src/components/Product/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import firestore from '@react-native-firebase/firestore'
 
 import { ButtonIcon } from '../ButtonIcon';
 import { Container, Info, Title, Quantity, Options } from './styles';
@@ -15,6 +16,16 @@ type Props = {
 }
 
 export function Product({ data }: Props) {
+  async function handleDoneToggle() {
+    const productsColletion = firestore().collection('products')
+    await productsColletion.doc(data.id).update({ done: !data.done })
+  }
+
+  async function handleDelete() {
+    const productsCollection = firestore().collection('products')
+    await productsCollection.doc(data.id).delete()
+  }
+  
   return (
     <Container>
       <Info>
@@ -30,11 +41,13 @@ export function Product({ data }: Props) {
       <Options>
         <ButtonIcon
           icon={data.done ? "undo" : "check"}
+          onPress={handleDoneToggle}
         />
 
         <ButtonIcon
           icon="delete"
           color="alert"
+          onPress={handleDelete}
         />
       </Options>
     </Container>
