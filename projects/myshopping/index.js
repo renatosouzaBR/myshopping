0b9076320addd49fac41e7a1451b6cfd9190aa86
renatosouzaBR <natoicm@gmail.com>
2022-09-10 23:01:25 -0300

    feat: add firestore emulator

--- projects/myshopping/index.js
+++ projects/myshopping/index.js
@@ -1,7 +1,25 @@
-import 'react-native-gesture-handler';
-import { registerRootComponent } from 'expo';
+import "react-native-gesture-handler";
+import "@react-native-firebase/app";
+import firestore from "@react-native-firebase/firestore";
+import { registerRootComponent } from "expo";
 
-import App from './App';
+if (__DEV__) {
+  firestore()
+    .terminate()
+    .then(() => {
+      firestore()
+        .clearPersistence()
+        .then(() => {
+          firestore().useEmulator("localhost", 8080);
+        })
+        .catch((error) => console.log("Clear Persistence Error: ", error));
+    })
+    .catch(() => console.log("Terminate Error"));
+}
+
+firestore();
+
+import App from "./App";
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
