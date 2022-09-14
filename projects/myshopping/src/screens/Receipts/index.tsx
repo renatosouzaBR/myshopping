76f9d3957182056d18cb7cf14edf935dd96e2b97
renatosouzaBR <natoicm@gmail.com>
2022-09-14 09:25:20 -0300

    feat: add fetch images

--- projects/myshopping/src/screens/Receipts/index.tsx
+++ projects/myshopping/src/screens/Receipts/index.tsx
@@ -1,37 +1,46 @@
-import React from 'react';
-import { FlatList } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { FlatList } from "react-native";
+import storage from "@react-native-firebase/storage";
 
-import { Container, PhotoInfo } from './styles';
-import { Header } from '../../components/Header';
-import { Photo } from '../../components/Photo';
-import { File } from '../../components/File';
-
-import { photosData } from '../../utils/photo.data';
+import { Container, PhotoInfo } from "./styles";
+import { Header } from "../../components/Header";
+import { Photo } from "../../components/Photo";
+import { File, FileProps } from "../../components/File";
 
 export function Receipts() {
+  const [files, setFiles] = useState<FileProps[]>([]);
+
+  async function fetchImages() {
+    const fetchedFiles = await storage().ref("/images").list();
+    const formattedFiles = fetchedFiles.items.map((image) => ({
+      name: image.name,
+      path: image.fullPath,
+    }));
+
+    setFiles(formattedFiles);
+  }
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
   return (
     <Container>
       <Header title="Comprovantes" />
 
       <Photo uri="" />
 
-      <PhotoInfo>
-        Informações da foto
-      </PhotoInfo>
+      <PhotoInfo>Informações da foto</PhotoInfo>
 
       <FlatList
-        data={photosData}
-        keyExtractor={item => item.name}
+        data={files}
+        keyExtractor={(item) => item.name}
         renderItem={({ item }) => (
-          <File
-            data={item}
-            onShow={() => { }}
-            onDelete={() => { }}
-          />
+          <File data={item} onShow={() => {}} onDelete={() => {}} />
         )}
         contentContainerStyle={{ paddingBottom: 100 }}
         showsVerticalScrollIndicator={false}
-        style={{ width: '100%', padding: 24 }}
+        style={{ width: "100%", padding: 24 }}
       />
     </Container>
   );
--- projects/myshopping/src/screens/Upload/index.tsx
+++ projects/myshopping/src/screens/Upload/index.tsx
@@ -58,7 +58,7 @@ export function Upload() {
 
   return (
     <Container>
-      <Header title="Lista de compras" />
+      <Header title="Lista de Fotos" />
 
       <Content>
         <Photo uri={image} onPress={handlePickImage} />
