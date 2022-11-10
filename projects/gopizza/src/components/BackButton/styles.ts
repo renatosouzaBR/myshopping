147defdc09637f5094ecb3ae5b1b6f0e660b64d7
Renato Alves Souza <renatoalvessouza@MacBook-Air-de-Renato.local>
2022-11-10 09:14:38 -0300

    feat: add BackButton component

---
+++ projects/gopizza/src/components/BackButton/styles.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components/native";
+import { TouchableOpacity } from "react-native";
+
+export const Container = styled(TouchableOpacity)`
+  width: 40px;
+  height: 40px;
+  border-radius: 12px;
+  border: 1px solid rgba(255, 255, 255, 0.24);
+
+  align-items: center;
+  justify-content: center;
+`;
