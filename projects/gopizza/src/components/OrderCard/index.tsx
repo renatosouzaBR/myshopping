a857f0302410fa30340bb511c0a1a6d166a9853e
Renato Souza <natoicm@gmail.com>
2022-11-16 16:06:17 -0300

    feat: fetch orders by waiter

--- projects/gopizza/src/components/OrderCard/index.tsx
+++ projects/gopizza/src/components/OrderCard/index.tsx
@@ -10,19 +10,33 @@ import {
   ContainerProps,
 } from "./styles";
 
-interface OrderCardProps extends ContainerProps {}
+export interface OrderProps {
+  id: string;
+  pizza: string;
+  photo_url: string;
+  size: string;
+  quantity: number;
+  table_number: string;
+  status: string;
+}
+
+interface OrderCardProps extends ContainerProps {
+  data: OrderProps;
+}
 
 export function OrderCard(props: OrderCardProps) {
-  const { index } = props;
+  const { index, data } = props;
 
   return (
     <Container index={index}>
-      <Photo source={{ uri: "https://github.com/renatosouzabr.png" }} />
-      <Name>4 Queijos</Name>
-      <Details>Mesa 01 • Qnt: 1</Details>
+      <Photo source={{ uri: data.photo_url }} />
+      <Name>{data.pizza}</Name>
+      <Details>
+        Mesa {data.table_number} • Qnt: {data.quantity}
+      </Details>
 
-      <StatusView status="Pronto">
-        <Status status="Pronto">Pronto</Status>
+      <StatusView status={data.status}>
+        <Status status={data.status}>{data.status}</Status>
       </StatusView>
     </Container>
   );
--- projects/gopizza/src/screens/Orders/index.tsx
+++ projects/gopizza/src/screens/Orders/index.tsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { FlatList } from "react-native";
+import firestore from "@react-native-firebase/firestore";
 
-import { OrderCard } from "@/components/OrderCard";
+import { useAuth } from "@/hooks/auth";
+
+import { OrderCard, OrderProps } from "@/components/OrderCard";
 
 import { Container, Header, Title, HorizontalLine } from "./styles";
 
 export function Orders() {
+  const { user } = useAuth();
+
+  const [orders, setOrders] = useState<OrderProps[]>([]);
+
+  useEffect(() => {
+    const subscribe = firestore()
+      .collection("orders")
+      .where("waiter_id", "==", user.id)
+      .onSnapshot((observer) => {
+        const data = observer.docs.map((order) => ({
+          id: order.id,
+          ...order.data(),
+        })) as OrderProps[];
+
+        setOrders(data);
+      });
+
+    return () => subscribe();
+  }, []);
+
   return (
     <Container>
       <Header>
@@ -13,9 +36,11 @@ export function Orders() {
       </Header>
 
       <FlatList
-        data={[1, 2, 3]}
-        keyExtractor={(item) => String(item)}
-        renderItem={({ item, index }) => <OrderCard index={index} />}
+        data={orders}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item, index }) => (
+          <OrderCard index={index} data={item} />
+        )}
         showsVerticalScrollIndicator={false}
         numColumns={2}
         ItemSeparatorComponent={HorizontalLine}
